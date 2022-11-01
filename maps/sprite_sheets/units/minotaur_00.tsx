6a1871dad23d98a69f877af65c9d1be90659d400
Nathanial <Nathanial28@gmail.com>
2022-11-01 16:53:41 -0600

    added more sprites

---
+++ maps/sprite_sheets/units/minotaur_00.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="minotaur_00" tilewidth="128" tileheight="128" tilecount="192" columns="24">
+ <image source="../../../sprites/units/monsters/minotaur/minotaur_00.png" width="3072" height="1024"/>
+</tileset>
