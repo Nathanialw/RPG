5bfb07d1b1f964ed7f36599671c937b75b5af32e
nathanial <nathanial28@gmail.com>
2024-03-05 19:36:18 -0700

    -now loads tileset objects by subtypes

--- src/Maps/Procedural_Generation/Labyrinth/treasure_room.h
+++ src/Maps/Procedural_Generation/Labyrinth/treasure_room.h
@@ -1,9 +1,8 @@
 #pragma once
 #include "labyrinth.h"
-
+#include <iterator>
 
 namespace Room {
-
 
   /* Returns an array of strings of types entities in the room
    * ie. chests, tombs etc
@@ -33,9 +32,16 @@ namespace Room {
     ///brazier
     //light
     if (tileType == 0) {
-      int MAX_SIZE = Game_Objects_Lists::tilesetObjects[tileset]["chest"].size();
-      int xmlIndex = Procedural_Generation::Random_Int(0, MAX_SIZE, seed);
-      return Game_Objects_Lists::tilesetObjects[tileset]["chest"][xmlIndex];
+
+      //      int MAX_TYPES = Game_Objects_Lists::tilesetObjects[tileset].size();
+      //      int randomType = Procedural_Generation::Random_Int(0, MAX_TYPES, seed);
+      //
+      //      int MAX_OBJECTS = Game_Objects_Lists::tilesetObjects[tileset][randomType].size();
+      //      int randomObject = Procedural_Generation::Random_Int(0, MAX_OBJECTS, seed);
+
+
+      //      return Game_Objects_Lists::tilesetObjects[tileset]["chest"][randomObject][direction];
+      return Game_Objects_Lists::tilesetObjects[tileset]["chest"]["Chest1"][0];
       //      int xmlIndex = Procedural_Generation::Random_Int(0, 10, seed);
       //place a chest in the middle of the room
       //get the boundaries of the room
--- src/SQLite/SQLite_load_tileset.h
+++ src/SQLite/SQLite_load_tileset.h
@@ -11,9 +11,10 @@ namespace SQLite_Load_Tileset {
     const std::string tilesetStr = db::Append_Quotes(tileset);
 
     std::vector<std::string> objectTypes;
-    objectTypes.reserve(200);
+    objectTypes.reserve(50);
     sqlite3_stmt *stmt0;
 
+    //returns subtypes available in the labyrinth
     std::string query0 = "SELECT DISTINCT subtype FROM building_exteriors WHERE tileset = " + tilesetStr;
     sqlite3_prepare_v2(db::db, query0.c_str(), -1, &stmt0, nullptr);
     while (sqlite3_step(stmt0) != SQLITE_DONE) {
@@ -21,20 +22,38 @@ namespace SQLite_Load_Tileset {
       objectTypes.emplace_back(db::Get_String(text));
     }
 
-    for (const auto &subtype: objectTypes) {
-      const std::string subtypeStr = db::Append_Quotes(subtype);
+    // returns names objects available in the labyrinth
+    std::vector<std::string> objectNames;
+    objectNames.reserve(50);
+    sqlite3_stmt *stmt2;
+    std::string query2 = "SELECT DISTINCT object_name FROM building_exteriors WHERE tileset = " + tilesetStr;
+    sqlite3_prepare_v2(db::db, query2.c_str(), -1, &stmt2, nullptr);
+    while (sqlite3_step(stmt2) != SQLITE_DONE) {
+      auto text = sqlite3_column_text(stmt2, 0);
+      objectNames.emplace_back(db::Get_String(text));
+    }
 
-      std::vector<std::string> tilesetVec;
-      tilesetVec.reserve(200);
-      sqlite3_stmt *stmt1;
-      std::string query1 = "SELECT name FROM building_exteriors WHERE tileset = " + tilesetStr + " AND subtype = " + subtypeStr;
-      sqlite3_prepare_v2(db::db, query1.c_str(), -1, &stmt1, nullptr);
-      while (sqlite3_step(stmt1) != SQLITE_DONE) {
-        auto text = sqlite3_column_text(stmt1, 0);
-        tilesetVec.emplace_back(db::Get_String(text));
+    // saves each object in vectors holding all available directions
+    for (const std::string &subtype: objectTypes) {
+      const std::string subtypeStr = db::Append_Quotes(subtype);
+      for (const std::string &object: objectNames) {
+        const std::string objectStr = db::Append_Quotes(object);
+
+        std::vector<std::string> tilesetVec;
+        tilesetVec.reserve(16);
+        sqlite3_stmt *stmt1;
+        std::string query1 = "SELECT name FROM building_exteriors WHERE tileset = " + tilesetStr + " AND subtype = " + subtypeStr + " AND object_name = " + objectStr;
+        sqlite3_prepare_v2(db::db, query1.c_str(), -1, &stmt1, nullptr);
+        while (sqlite3_step(stmt1) != SQLITE_DONE) {
+          auto text = sqlite3_column_text(stmt1, 0);
+          tilesetVec.emplace_back(db::Get_String(text));
+        }
+
+        if (!tilesetVec.empty()) {
+          tilesetVec.shrink_to_fit();
+          Game_Objects_Lists::tilesetObjects[tileset][subtype][object] = tilesetVec;
+        }
       }
-      tilesetVec.shrink_to_fit();
-      Game_Objects_Lists::tilesetObjects[tileset][subtype] = tilesetVec;
     }
   }
 
