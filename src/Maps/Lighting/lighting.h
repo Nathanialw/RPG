254a54359826120db95bfab6554894ff569606c3
nathanial <nathanial28@gmail.com>
2024-12-10 21:33:33 -0700

    Lighting now calculates and renders faster.

--- src/Maps/Lighting/lighting.h
+++ src/Maps/Lighting/lighting.h
@@ -11,14 +11,15 @@ namespace Lighting {
 	float lightRadiusF = 64.0f;
 	Uint8 maxDarkness = 250;
 	Uint8 minDarkness = 100;
+    SDL_Texture *radii[35];
 	SDL_Texture *texture;
+	SDL_Texture* base;
 	std::vector<Uint32> fogOfWarPixels;
 	bool setFogOfWar = false;
 
 	void Sight_Increase(entt::registry &zone) {
 		std::for_each(zone.view<Component::Light_Radius, Component::Input>().begin(), zone.view<Component::Light_Radius, Component::Input>().end(), [&](const auto &item) {
 			auto &lightRadius = zone.get<Component::Light_Radius>(item).lightRadius;
-			setFogOfWar = false;
 			lightRadius++;
 			if (lightRadius >= 35)
 				lightRadius = 35;
@@ -28,88 +29,99 @@ namespace Lighting {
 	void Sight_Decrease(entt::registry &zone) {
 		std::for_each(zone.view<Component::Light_Radius, Component::Input>().begin(), zone.view<Component::Light_Radius, Component::Input>().end(), [&](const auto &item) {
 			auto &lightRadius = zone.get<Component::Light_Radius>(item).lightRadius;
-			setFogOfWar = false;
 			lightRadius--;
 			if (lightRadius <= 5)
 				lightRadius = 5;
 		});
 	}
 
-	void Set_Pixels(int x, int y, int viewW, int lightStr) {
-		Uint8 red = 0;
-		Uint8 green = 0;
-		Uint8 blue = 0;
+    void Create_Texture(std::vector<Uint32> &pixelArray, SDL_Texture *lightRadius) {
+        if (!lightRadius) {
+            std::cerr << "Invalid texture: texture is null" << std::endl;
+            return;
+        }
 
-		int radiusW =  lightStr * (lightRadiusF * 0.75f);
-		int radiusH =  lightStr * (lightRadiusF * 0.5f);
+        void* pixels;
+        int pitch;
+        if (SDL_LockTexture(lightRadius, nullptr, &pixels, &pitch) != 0) {
+            std::cerr << "Failed to lock texture: " << SDL_GetError() << std::endl;
+            return;
+        }
 
-		int originX = x - (radiusW * 0.5f);
-		int originY = y - (radiusH * 0.5f);
-		int width = x + (radiusW * 0.5f);
-		int height = y + (radiusH * 0.5f);
+        auto* dst = (Uint32*)pixels;
+        std::copy(pixelArray.begin(), pixelArray.end(), dst);
 
-		for (int i = originY; i < height; i++) {
-			for (int j = originX; j < width; j++) {
-				int index = i * viewW + j;
+        SDL_UnlockTexture(lightRadius);
+    }
 
-				if (index < 0 || index >= fogOfWarPixels.size())
-					continue;
+    void  Set_BlendMode(SDL_Texture *Ltexture) {
+        SDL_BlendMode blendMode = SDL_ComposeCustomBlendMode(
+                SDL_BLENDFACTOR_ONE, SDL_BLENDFACTOR_ONE, SDL_BLENDOPERATION_MINIMUM,
+                SDL_BLENDFACTOR_ONE, SDL_BLENDFACTOR_ONE, SDL_BLENDOPERATION_MINIMUM
+        );
+        if (SDL_SetTextureBlendMode(Ltexture, blendMode) != 0) {
+            std::cerr << "Failed to set blend mode: " << SDL_GetError() << std::endl;
+            return;
+        }
+    }
 
-				if (i < 0 || i >= viewW || j < 0 || j >= viewW)
-					continue;
+    void Create_Light_radius(int lightStr) {
+        if (radii[lightStr])
+            return;
 
-				Uint8 darkness;
-				float dx = (j - x) / (lightStr * 0.75f);
-				float dy = (i - y) / (lightStr * 0.5f);
-				float distance = (dx * dx) + (dy * dy);
+        Uint8 red = 0;
+        Uint8 green = 0;
+        Uint8 blue = 0;
 
-				if (distance <= 1.0f)
-					fogOfWarPixels[index] = (minDarkness << 24) | (red << 16) | (green << 8) | blue;
-				else if (distance > (lightRadiusF * 0.5f) * (lightRadiusF * 0.5f))
-					continue;
-				else {
-					darkness = (Uint8)(minDarkness + sqrt(distance) * (maxDarkness - minDarkness) / (lightRadiusF * 0.5f));
-					Uint8 existingDarkness = fogOfWarPixels[index] >> 24;
+        int radiusW =  lightStr * (lightRadiusF * 0.75f);
+        int radiusH =  lightStr * (lightRadiusF * 0.5f);
 
-					if (existingDarkness < darkness)
-						darkness = existingDarkness;
+        int width = radiusW;
+        int height = radiusH;
 
-					Uint32 color = (darkness << 24) | (red << 16) | (green << 8) | blue;
-					fogOfWarPixels[index] = color;
-				}
-			}
-		}
-	}
+        std::vector<Uint32> lightRadiusPixels(width*height);
+        radii[lightStr] = SDL_CreateTexture(Graphics::renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+        Set_BlendMode(radii[lightStr]);
 
-	void Set_Screen() {
-		if (!texture) {
-			std::cerr << "Invalid texture: texture is null" << std::endl;
-			return;
-		}
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int index = i * width + j;
 
-		void* pixels;
-		int pitch;
-		if (SDL_LockTexture(texture, nullptr, &pixels, &pitch) != 0) {
-			std::cerr << "Failed to lock texture: " << SDL_GetError() << std::endl;
-			return;
-		}
+                Uint8 darkness;
+                float dx = (j - (width * 0.5f)) / (lightStr * 0.75f);
+                float dy = (i  - (height * 0.5f)) / (lightStr * 0.5f);
+                float distance = (dx * dx) + (dy * dy);
 
-		auto* dst = (Uint32*)pixels;
-		std::copy(fogOfWarPixels.begin(), fogOfWarPixels.end(), dst);
+                if (distance <= 1.0f)
+                    lightRadiusPixels[index] = (minDarkness << 24) | (red << 16) | (green << 8) | blue;
+                else if (distance > (lightRadiusF * 0.5f) * (lightRadiusF * 0.5f))
+                    lightRadiusPixels[index] = (maxDarkness << 24) | (red << 16) | (green << 8) | blue;
+                else {
+                    darkness = (Uint8)(minDarkness + (sqrt(distance) * (maxDarkness - minDarkness) / (lightRadiusF * 0.5f)));
+                    Uint32 color = (darkness << 24) | (red << 16) | (green << 8) | blue;
+                    lightRadiusPixels[index] = color;
+                }
+//                    lightRadiusPixels[index] = (minDarkness << 24) | (red << 16) | (green << 8) | blue;
+            }
+        }
 
-		SDL_UnlockTexture(texture);
-	}
+        std::cout << "Creating texture for light radius: " << lightRadiusPixels.size() << " " << lightStr << std::endl;
+
+        Create_Texture(lightRadiusPixels, radii[lightStr]);
+    }
 
 	void Init(Component::Camera &camera) {
 		if (!setFogOfWar) {
-			if (texture)
-				SDL_DestroyTexture(texture);
-			texture = SDL_CreateTexture(Graphics::renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, camera.screen.w, camera.screen.h);
-			if (!texture) {
-				std::cerr << "Failed to create texture: " << SDL_GetError() << std::endl;
-				return;
-			}
-			SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+            if (base)
+                SDL_DestroyTexture(base);
+
+            base = SDL_CreateTexture(Graphics::renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, camera.screen.w, camera.screen.h);
+
+            if (!base) {
+                std::cerr << "Failed to create texture: " << SDL_GetError() << std::endl;
+                return;
+            }
+
 			Uint8 alpha = maxDarkness;
 			Uint8 red = 0;
 			Uint8 green = 0;
@@ -120,37 +132,53 @@ namespace Lighting {
 				pixel = color;
 			fogOfWarPixels.shrink_to_fit();
 			setFogOfWar = true;
+
+            Create_Texture(fogOfWarPixels, base);
 		}
 	}
 
+    void Draw_Light_Radius(float x, float y, float lightStr, SDL_Texture* lightRadius) {
+        SDL_SetRenderTarget(Graphics::renderer, texture);
+        SDL_FRect  rect = {x - ((lightStr * (lightRadiusF * 0.75f)) * 0.5f), y - ((lightStr * (lightRadiusF * 0.5f)) * 0.5f), lightStr * (lightRadiusF * 0.75f), lightStr * (lightRadiusF * 0.5f)};
+        SDL_RenderCopyF(Graphics::renderer, lightRadius, nullptr, &rect);
+        SDL_SetRenderTarget(Graphics::renderer, nullptr);
+    }
+
 	void Display(Component::Camera &camera) {
+        SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
 		SDL_FRect rect = {0, 0, camera.screen.w, camera.screen.h};
 		SDL_RenderCopyF(Graphics::renderer, texture, nullptr, &rect);
 	}
 
 	void Get_Emitters(entt::registry &zone, Component::Camera &camera) {
-		Uint8 alpha = maxDarkness;
-		Uint8 red = 0;
-		Uint8 green = 0;
-		Uint8 blue = 0;
-		Uint32 color = (alpha << 24) | (red << 16) | (green << 8) | blue;
-		fogOfWarPixels.resize(camera.screen.w * camera.screen.h, color);
-		for (auto &pixel : fogOfWarPixels)
-			pixel = color;
+        if (texture)
+            SDL_DestroyTexture(texture);
+
+        texture = SDL_CreateTexture(Graphics::renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, camera.screen.w, camera.screen.h);
+        if (!texture) {
+            std::cerr << "Failed to create texture: " << SDL_GetError() << std::endl;
+            return;
+        }
+        SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderTarget(Graphics::renderer, texture);
+        SDL_RenderCopyF(Graphics::renderer, base, nullptr, nullptr);
+        SDL_SetRenderTarget(Graphics::renderer, nullptr);
+
+        Set_BlendMode(texture);
 
 		auto view = zone.view<Component::Position, Component::Light_Radius, Component::Renderable>();
 		for (auto entity : view) {
 			auto light = view.get<Component::Light_Radius>(entity);
 			auto position = view.get<Component::Position>(entity);
 			auto screenPosition = Camera_Control::Convert_Position_To_Screen_Coods(camera, position);
-			Set_Pixels(screenPosition.x, screenPosition.y, camera.screen.w, light.lightRadius);
+            Create_Light_radius(light.lightRadius);
+			Draw_Light_Radius(screenPosition.x, screenPosition.y, light.lightRadius, radii[light.lightRadius]);
 		}
 	}
 
 	void Render(entt::registry &zone, Component::Camera &camera) {
 		Init(camera);
 		Get_Emitters(zone, camera);
-		Set_Screen();
 		Display(camera);
 	}
 
